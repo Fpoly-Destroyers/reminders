c03892105a54e31a40809e2d8e8a26feef855e41
Cactus137 <Anhdo13072004@gmail.com>
2024-08-03 15:20:35 +0700

    feature: migrate done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('fullname');
+            $table->string('avatar')->default('profile.png');
+            $table->tinyInteger('is_active')->default(1)->comment('0: inactive, 1: active');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
