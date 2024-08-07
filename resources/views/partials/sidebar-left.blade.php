d0a0ec85e60699839694056f64b80bef39b9af00
Đỗ Hồng Quân <quandohong28@192.168.0.100>
2024-08-07 15:18:32 +0700

    feat:ui fix bugs appearance

--- resources/views/partials/sidebar-left.blade.php
+++ resources/views/partials/sidebar-left.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 border-r border-gray-200 h-screen bg-gray-100">
+<div class="p-4 border-r border-gray-200 h-screen bg-gray-100 overflow-y-auto no-scrollbar">
     <div class="mb-4">
         @livewire('search')
     </div>
