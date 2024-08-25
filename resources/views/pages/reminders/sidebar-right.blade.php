ec5437201f68450b1a44244461735f3301267dbc
Cactus137 <Anhdo13072004@gmail.com>
2024-08-25 12:37:07 +0700

    fix some bug

--- resources/views/pages/reminders/sidebar-right.blade.php
+++ resources/views/pages/reminders/sidebar-right.blade.php
@@ -1,7 +1,7 @@
 <div class="h-screen p-4 flex justify-between items-start">
     {{-- Render component tuy theo route --}}
     <div class="w-full">
-        {{-- @livewire('add-folder') --}}
-        @livewire('edit-folder', ['slug' => $slug ?? null])
+        @livewire('add-folder')
+        {{-- @livewire('edit-folder', ['slug' => $slug ?? null]) --}}
     </div>
 </div>
